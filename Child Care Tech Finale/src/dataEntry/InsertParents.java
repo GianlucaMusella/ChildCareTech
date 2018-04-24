d2d797123282f6cfb60b11d226e8081e1f546ace
GianlucaMusella <36888190+GianlucaMusella@users.noreply.github.com>
2018-04-24 16:50:25 +0200

    Add files via upload

--- Child Care Tech Finale/src/dataEntry/InsertParents.java
+++ Child Care Tech Finale/src/dataEntry/InsertParents.java
@@ -9,17 +9,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import loginScreen.Singleton;
 import serverRMI.InterfaceRMI;
 
-import java.awt.*;
 import java.time.LocalDate;
 
-
-public class NewChild {
+public class InsertParents {
 
     @FXML
     private TextField txtNome;
@@ -31,31 +28,16 @@ public class NewChild {
     private TextField txtCodiceFiscale;
 
     @FXML
-    private TextField txtLuogo;
-
-    @FXML
     private DatePicker dateData;
 
     @FXML
-    private TextField txtGenitore1;
+    private TextField txtLuogo;
 
     @FXML
-    private TextField txtGenitore2;
-
-    @FXML
-    private TextField txtPediatra;
-
-    @FXML
-    private TextField txtAllergia;
-
-    @FXML
-    private TextField txtIDBambino;
+    private TextField txtTelefono;
 
     @FXML
     private TextField txtSesso;
-
-    @FXML
-    private TextField txtContatto;
 
     @FXML
     private TableView tableBambini;
@@ -69,22 +51,18 @@ public class NewChild {
     @FXML
     private TableColumn columnCodiceFiscale;
 
-    public void addChild(ActionEvent actionEvent) throws Exception {
+    public void addParents(ActionEvent actionEvent) throws Exception {
 
         String nome = txtNome.getText();
         String cognome = txtCognome.getText();
         String codiceFiscale = txtCodiceFiscale.getText();
         String luogo = txtLuogo.getText();
         LocalDate data = dateData.getValue();
-        String genitore1 = txtGenitore1.getText();
-        String genitore2 = txtGenitore2.getText();
-        String pediatra = txtPediatra.getText();
-        String allergie = txtAllergia.getText();
-        String idBambino = txtIDBambino.getText();
         String sesso = txtSesso.getText();
+        String telefono = txtTelefono.getText();
 
         InterfaceRMI interfaceRMI = Singleton.getInstance().rmiLookup();
-        boolean success = interfaceRMI.addChild(codiceFiscale, idBambino,  nome,  cognome,  data,  luogo,  allergie,  genitore1,  genitore2, sesso, pediatra);
+        boolean success = interfaceRMI.addParents(codiceFiscale,  nome,  cognome,  data,  luogo, telefono, sesso);
 
         //se metti il cambio del label serve la try catch con remoteexception
 
@@ -93,25 +71,11 @@ public class NewChild {
         txtCognome.clear();
         txtCodiceFiscale.clear();
         txtLuogo.clear();
-
-        txtGenitore1.clear();
-        txtGenitore2.clear();
-        txtPediatra.clear();
-        txtAllergia.clear();
-        txtIDBambino.clear();
+        txtTelefono.clear();
         txtSesso.clear();
 
     }
 
-    public void inserisciGenitore(ActionEvent actionEvent) throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getResource("/dataEntry/InsertParents.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-
-    }
 
     public void back_method(ActionEvent actionEvent) throws Exception{
 
