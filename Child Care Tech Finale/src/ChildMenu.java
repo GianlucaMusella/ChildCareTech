500e13ed772e087d307576b59e9a46e34be49383
GianlucaMusella <36888190+GianlucaMusella@users.noreply.github.com>
2018-04-24 16:53:57 +0200

    Add files via upload

---
+++ Child Care Tech Finale/src/ChildMenu.java
@@ -0,0 +1,47 @@
+package menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import loginScreen.Controller;
+
+public class ChildMenu {
+
+    public void inserisciBambino(ActionEvent actionEvent) throws Exception {
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/dataEntry/NewChild.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void cercaBambino(ActionEvent actionEvent) throws Exception {
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/delete/SearchPerson.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void back_method(ActionEvent actionEvent) throws Exception{
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/menu/Choice.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void logout(ActionEvent ev) throws Exception {
+        ((Node) ev.getSource()).getScene().getWindow().hide();
+        Controller controller = new Controller();
+        Stage stage = new Stage();
+        controller.initialize(stage);
+    }
+
+}
---
+++ Child Care Tech Finale/src/Choice.java
@@ -0,0 +1,46 @@
+package menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import loginScreen.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Choice{
+
+    public void inserisciPersona(ActionEvent actionEvent) throws Exception {
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/menu/InsertPerson.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void inserisciFornitore(ActionEvent actionEvent) throws Exception {
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/menu/InsertSupplier.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void logout(ActionEvent ev) throws Exception {
+        ((Node) ev.getSource()).getScene().getWindow().hide();
+        Controller controller = new Controller();
+        Stage stage = new Stage();
+        controller.initialize(stage);
+    }
+
+}
