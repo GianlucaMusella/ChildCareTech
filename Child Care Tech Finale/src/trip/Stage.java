9dce303c7830eb16d9ec13deb24659e212d2ba02
GianlucaMusella <36888190+GianlucaMusella@users.noreply.github.com>
2018-05-18 15:19:27 +0200

    Fine Gita

---
+++ Child Care Tech Finale/src/trip/Stage.java
@@ -0,0 +1,51 @@
+package trip;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import main.Singleton;
+import serverRMI.InterfaceRMI;
+
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class Stage {
+
+    @FXML
+    private TextField txtIdTappa;
+
+    @FXML
+    private TextField txtTappa;
+
+    @FXML
+    private DatePicker giornoTappa;
+
+    @FXML
+    private TextField txtidGita;
+
+    @FXML
+    private TextField txtOra;
+
+
+    public void newTappa (ActionEvent actionEvent) throws Exception {
+
+        String numeroTappa = txtIdTappa.getText();
+        String tappa = txtTappa.getText();
+        String idGita = txtidGita.getText();
+        LocalDate giorno = giornoTappa.getValue();
+        String ora = txtOra.getText();
+
+        InterfaceRMI interfaceRMI = Singleton.getInstance().rmiLookup();
+        interfaceRMI.newTappaServer(numeroTappa, tappa, idGita, giorno, ora);
+
+
+        txtidGita.clear();
+        txtIdTappa.clear();
+        txtTappa.clear();
+        txtOra.clear();
+        giornoTappa.getEditor().clear();
+    }
+
+
+}
