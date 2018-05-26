fb456c06f1f6a9b86c15738a099b5d3b621d71a3
Luca <luca.tussi@live.com>
2018-05-26 09:01:48 +0200

    Ho sistemato il check delle allergie. Ora come parametri gli passo primo, secondo e contorno (ho dovuto cambiare un po' di roba) e si può fare la ricerca inserendo anche uno solo. Prova a vedere se funziona

--- Child Care Tech Finale/src/interfaces/InterfaceServer.java
+++ Child Care Tech Finale/src/interfaces/InterfaceServer.java
@@ -56,7 +56,7 @@ public interface InterfaceServer extends Remote{
     ArrayList<SecondDishGS> viewSecond() throws Exception;
     ArrayList<SideDishGS> viewSide() throws Exception;
     ArrayList<MenuGS> viewMenu() throws Exception;
-    ArrayList<BambiniAllergici> viewCheck(String nomeMenu) throws Exception;
+    ArrayList<BambiniAllergici> viewCheck(String primo, String secondo, String contorno) throws Exception;
     boolean addPrimo(String nome, String allergeni) throws Exception;
     boolean addSecondo(String nome, String allergeni) throws Exception;
     boolean addSide(String nome, String allergeni) throws Exception;
