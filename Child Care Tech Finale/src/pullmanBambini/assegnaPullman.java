5e61bf797e0f08303bcfdf37e8658a192f55e28e
Luca <luca.tussi@live.com>
2018-05-28 09:20:33 +0200

    ASSEGNA NUMERO PULLMA

---
+++ Child Care Tech Finale/src/pullmanBambini/assegnaPullman.java
@@ -0,0 +1,27 @@
+package pullmanBambini;
+
+import connectionDatabase.ConnectionDatabase;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// viene chiamata quando un viene inserito un nuovo partecipante
+public class assegnaPullman {
+    // da lato client prende in input l'idigta e il codice fiscale del bambino. Chiamata subito dopo newPartecipante e countPullman
+    // LATO SERVER
+    public void assegnaPullman (String codicefiscale, String idGita) throws Exception {
+        ConnectionDatabase connectionDatabase = new ConnectionDatabase();
+        Statement stmt = connectionDatabase.initializeConnection().createStatement();
+        // int i = 0; // questo sarà il numero del pullman
+        String PullmanSQL = ("SELECT * FROM mydb.bambini_has_gita WHERE mydb.bambini_has_gita.Gita_idGIta = ");
+        ResultSet rsbis = stmt.executeQuery(PullmanSQL + idGita);
+        float counter = 0.0f;
+        while (rsbis.next()){
+            counter = counter + 1;
+        }
+        float numPullman = (float) (counter / 1.00);
+        String SQL = ("UPDATE Bambini_has_gita SET numPullman = " + numPullman + " WHERE Bambini_CodiceFiscale = '" +
+                codicefiscale  + "' AND Gita_idGIta = '" + idGita + "'");
+        int i = stmt.executeUpdate(SQL);
+    }
+}
